052b8efbf38de2c77aae75ab0c1fc571b30f55c5
hungnh <nhhung125@gmail.com>
2023-04-05 20:09:41 +0700

    update

---
+++ pratice/test/set.py
@@ -0,0 +1,54 @@
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(0)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+# Loop Through a List
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+#
+# newlist = [x for x in fruits if "a" in x]
+#
+# print(newlist)
+# newlist = [x for x in fruits if x != "apple"]
+# print(newlist)
+#
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+#
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# print(thislist)
+
+# def myfunc(n):
+#   return abs(n - 50)
+#
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key = myfunc)
+# print(thislist)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pratice/test/test.py
@@ -0,0 +1,26 @@
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+#
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+#
+#
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
+#
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
+#
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+thisset = {"apple", "banana", "cherry"}
+thistuple = ("kiwi", "orange")
+# thislist.extend(thistuple)
+# print(thislist)
